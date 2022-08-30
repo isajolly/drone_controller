f2ccbd9e2c2a8f568c1439398513545c3d2c22bc
isajolly <74981888+isajolly@users.noreply.github.com>
2022-08-30 14:48:03 +0200

    Update test_imu.ino

--- test_imu.ino
+++ test_imu.ino
@@ -8,75 +8,83 @@
 // on-board LED ring, at PORT0 (onboard)
 MeRGBLed led( 0, LEDNUM );
 
-float j, f, k;
+float x,y;
+int passage=0;
 
-MeGyro gyro_ext(0, 0x68); //external gryo sensor
 MeGyro gyro(1, 0x69);
-MeGyro gyro2(1, 0x69);
 
 void setup()
 {
   led.setpin( 44 );
-  Serial.begin(115200);
+  Serial.begin(9600);
   gyro.begin();
-  gyro2.begin();
-  Serial.print("X:");
-  Serial.print(gyro.getAngleX() );
-  Serial.print(" Y:");
-  Serial.print(gyro.getAngleY() );
-  Serial.print(" Z:");
-  Serial.println(gyro.getAngleZ() );
+
+
 }
 
 void loop()
 {
-  gyro.fast_update();
-  gyro2.update();
+  gyro.update();
   color_loop();
-  Serial.read();
-  Serial.print("X:");
-  Serial.print(gyro.getAngleX() );
-  Serial.print(" Y:");
-  Serial.print(gyro.getAngleY() );
-  Serial.print(" Z:");
-  Serial.println(gyro.getAngleZ() );
-  Serial.print("X2:");
-  Serial.print(gyro.getAngleX() );
-  Serial.print(" Y2:");
-  Serial.print(gyro.getAngleY() );
-  Serial.print(" Z2:");
-  Serial.println(gyro.getAngleZ() );
+
+    if (passage==80){
+      Serial.println("-----------");
+      Serial.print("X:");
+      Serial.println(gyro.getAngleX() );
+      Serial.print(" Y:");
+      Serial.println(gyro.getAngleY() );
+      Serial.print(" Z:");
+      Serial.println(gyro.getAngleZ() );
+      passage=0;
+    }else {passage+=1;}
+
   delay(10);
 }
 
 void color_loop()
 {
-  float a = max(min(100 + 20 * gyro.getAngleX(), 255), 0);
-  float b = max(min(100 - 20 * gyro.getAngleX(), 255), 0);
-  float c = max(min(100 + 20 * gyro.getAngleY(), 255), 0);
-  float d = max(min(100 - 20 * gyro.getAngleY(), 255), 0);
+  x = gyro.getAngleX();
+  y= gyro.getAngleY();
 
-  led.setColorAt( 1, a, b, 30 );
-  led.setColorAt( 2, a - 20, b + 20, 30 );
-  led.setColorAt( 12, a + 20, b - 20, 30 );
-  led.setColorAt( 3, c - 20, d + 20, 30 );
-  led.setColorAt( 4, c, d, 30 );
-  led.setColorAt( 5, c + 20, d - 20, 30 );
-  led.setColorAt( 6, b - 20, a + 20, 30 );
-  led.setColorAt( 7, b, a, 30 );
-  led.setColorAt( 8, b + 20, a - 20, 30 );
-  led.setColorAt( 9, d - 20, c + 20, 30 );
-  led.setColorAt( 10, d, c, 30 );
-  led.setColorAt( 11, d + 20, c - 20, 30 );
+  if (fabs(x)<3. && fabs(y)<3.){no_color(); }
+  else if (x>0.){
+    if(y>0.){
+      if(x>y){color_fill(2);}
+      else{color_fill(5);}
+    }
+    else{
+      if(x>fabs(y)){color_fill(2);}
+      else{color_fill(11);}
+    }
+  }
+  else{
+    if(y>0.){
+      if(fabs(x)>y){color_fill(8);}
+      else{color_fill(5);}
+    }
+    else{
+      if(fabs(x)>fabs(y)){color_fill(8);}
+      else{color_fill(11);}
+    }
+  }
+  
 
 
-  //  for (uint8_t t = 0; t < LEDNUM; t++ )
-  //  {
-  //    uint8_t red = 64 * (1 + sin(t / 2.0 + j / 4.0) );
-  //    uint8_t green = 64 * (1 + sin(t / 1.0 + f / 9.0 + 2.1) );
-  //    uint8_t blue = 64 * (1 + sin(t / 3.0 + k / 14.0 + 4.2) );
-  //    led.setColorAt( t, red, green, blue );
-  //  }
   led.show();
 
+}
+
+void no_color(){
+   for (uint8_t t = 0; t < LEDNUM; t++ )
+   {
+     led.setColorAt( t, 0,0,0 );
+   }
+}
+
+void color_fill(int place){
+  for (uint8_t t = 0; t < LEDNUM; t++ )
+   {
+    if(t==place){ led.setColorAt( t, 200,0,50 );}
+    else{ led.setColorAt( t, 0,0,0 );}
+   }
 }
