ed17ba636bbca582bd5d407c42c1da0734e9f7f5
isajolly <74981888+isajolly@users.noreply.github.com>
2022-09-08 15:24:45 +0200

    Update courbe_moteur.py

--- courbe_moteur.py
+++ courbe_moteur.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fichiers=["ESC06 - Foglio1.csv","ESC07 - Foglio1.csv","ESC13 - Foglio1.csv","ESC81 - Foglio1.csv"]
+#fichiers=["motor-2-16.csv","motor-2-2.csv","motor-2-14.csv","motor-2-13.csv"]
 
 f0=open(fichiers[0], newline='')
 f1=open(fichiers[1], newline='')
@@ -15,6 +16,34 @@ reader1= csv.DictReader(f1, delimiter=',')
 reader2= csv.DictReader(f2, delimiter=',')
 reader3= csv.DictReader(f3, delimiter=',')
 
+MotPowMin=1000
+MotPowMax=2000
+MotExp=0.
+SpinMin=1234
+SpinMax=1950
+CommandIdle=1150
+CommandMax=2000
+MotExp1=0.
+SpinMin1=1150
+SpinMax1=1950
+ThrustSpinMin1=0.271
+ThrustSpinMax1=1.881383512
+MotExp2=0.
+SpinMin2=1217
+SpinMax2=1950
+ThrustSpinMin2=0.271
+ThrustSpinMax2=1.881383512
+MotExp3=0.
+SpinMin3=1234
+SpinMax3=1950
+ThrustSpinMin3=0.271
+ThrustSpinMax3=1.881383512
+
+color0='hotpink'
+color1='violet'
+color2='palegreen'
+color3='lightskyblue'
+
 #---------MOTOR 1--------------
 
 ESCSignal=[]
@@ -22,13 +51,6 @@ Thrust=[]
 Voltage=[]
 Current=[]
 
-MotPowMin=1000
-MotPowMax=2000
-MotExp=0.3
-SpinMin=1150
-SpinMax=1950
-CommandIdle=1150
-CommandMax=2000
 
 for row in reader0:
 	ESCSignal.append(float(row['ESC']))
@@ -42,6 +64,8 @@ for i in range(len(ESCSignal)) :
 	c=max(0,(ESCSignal[i]-CommandIdle)/(CommandMax-CommandIdle))
 	p=max(0,(1-MotExp)*c+MotExp*c*c)
 	value=SpinMin+p*(2000-SpinMin)
+	value= 1249.5+(ESCSignal[i]-1000)*0.6105 #drone orange
+	value= 1065+(ESCSignal[i]-1000)*0.73 #drone noir
 	Corr.append(value)
 	a=2000
 	res=0
@@ -58,12 +82,6 @@ Thrust1=[]
 Voltage1=[]
 Current1=[]
 
-MotExp1=0.3
-SpinMin1=1150
-SpinMax1=1950
-ThrustSpinMin1=0.271
-ThrustSpinMax1=1.881383512
-
 
 for row in reader1:
 	ESCSignal1.append(float(row['ESC']))
@@ -77,6 +95,8 @@ for i in range(len(ESCSignal1)) :
 	c=max(0,(ESCSignal1[i]-CommandIdle)/(CommandMax-CommandIdle))
 	p=max(0,(1-MotExp1)*c+MotExp1*c*c)
 	value=SpinMin1+p*(2000-SpinMin1)
+	value= 1152+(ESCSignal1[i]-1000)*0.827 #drone orange
+	value= 1125+(ESCSignal1[i]-1000)*0.69 #drone noir
 	Corr1.append(value)
 	a=2000
 	res=0
@@ -93,12 +113,6 @@ Thrust2=[]
 Voltage2=[]
 Current2=[]
 
-MotExp2=0.3
-SpinMin2=1150
-SpinMax2=1950
-ThrustSpinMin2=0.271
-ThrustSpinMax2=1.881383512
-
 
 for row in reader2:
 	ESCSignal2.append(float(row['ESC']))
@@ -112,6 +126,8 @@ for i in range(len(ESCSignal2)) :
 	c=max(0,(ESCSignal2[i]-CommandIdle)/(CommandMax-CommandIdle))
 	p=max(0,(1-MotExp2)*c+MotExp2*c*c)
 	value=SpinMin2+p*(2000-SpinMin2)
+	value= 1190+(ESCSignal2[i]-1000)*0.689 #drone orange
+	value= 1056+(ESCSignal2[i]-1000)*0.86 #drone noir
 	Corr2.append(value)
 	a=2000
 	res=0
@@ -127,12 +143,6 @@ Thrust3=[]
 Voltage3=[]
 Current3=[]
 
-MotExp3=0.3
-SpinMin3=1150
-SpinMax3=1950
-ThrustSpinMin3=0.271
-ThrustSpinMax3=1.881383512
-
 
 for row in reader3:
 	ESCSignal3.append(float(row['ESC']))
@@ -146,6 +156,8 @@ for i in range(len(ESCSignal3)) :
 	c=max(0,(ESCSignal3[i]-CommandIdle)/(CommandMax-CommandIdle))
 	p=max(0,(1-MotExp3)*c+MotExp3*c*c)
 	value=SpinMin3+p*(2000-SpinMin3)
+	value=1104+0.53*(ESCSignal3[i]-1000) #drone orange
+	value=1063+0.73*(ESCSignal3[i]-1000) #drone noir
 	Corr3.append(value)
 	a=2000
 	res=0
@@ -154,13 +166,27 @@ for i in range(len(ESCSignal3)) :
 			res=Thrust3[j]
 			a=abs(value-ESCSignal3[j])
 	New3.append(res)
+	
 
 if __name__=="__main__":
 
-	plt.plot(ESCSignal,New)
-	plt.plot(ESCSignal1,New1)
-	plt.plot(ESCSignal2,New2)
-	plt.plot(ESCSignal3,New3)
+	plt.plot(ESCSignal,Thrust, color=color0)
+	plt.plot(ESCSignal1,Thrust1, color=color1)
+	plt.plot(ESCSignal2,Thrust2, color=color2)
+	plt.plot(ESCSignal3,Thrust3, color=color3)
+	plt.plot([1050+10*i for i in range(85)],[620 for i in range(85)], color= 'red') #drone noir
+	plt.plot([1050+10*i for i in range(85)],[34 for i in range(85)], color= 'red') #drone noir
+	#plt.plot([1100+10*i for i in range(85)],[432 for i in range(85)], color= 'red') #drone orange
+	
+	#plt.plot([1100+10*i for i in range(85)],[60 for i in range(85)], color= 'red') #drone orange
+	plt.show()
+	
+	plt.plot(ESCSignal,New, color=color0)
+	plt.plot(ESCSignal1,New1, color=color1)
+	plt.plot(ESCSignal2,New2, color=color2)
+	plt.plot(ESCSignal3,New3, color=color3)
+	#plt.plot([1150+10*i for i in range(85)],[8*i for i in range(50)], color= 'red') #drone noir
+	#plt.plot([1050+10*i for i in range(90)],[50+4*i for i in range(90)], color= 'red') #drone orange
 	plt.show()
 
 	"""
